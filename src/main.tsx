03022c2fa6ad68fac55f5a0fb66e616071750fed
Numonu <juan.villegas.dev@gmail.com>
2023-08-06 20:55:28 -0500

    update:import file dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './global/styles/defaults.css'
+import Routing from './pages/routing'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1 className="text-emerald-500 text-4xl">Hello World</h1>
+    <Routing/>
   </React.StrictMode>,
 )
