d62765324a811392fd7457236d7f01a65e48fa99
Numonu <juan.villegas.dev@gmail.com>
2023-08-07 10:06:33 -0500

    update:updatefile dependencies to main script

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./global/styles/defaults.css";
+import "./global/styles/animations.css";
 import Routing from "./pages/routing";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
