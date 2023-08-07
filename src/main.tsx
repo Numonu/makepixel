a3c906ebd4821836e9677e2fa0c49747c52f6b1c
Numonu <juan.villegas.dev@gmail.com>
2023-08-06 20:40:43 -0500

    build:install and setup tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './global/styles/defaults.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <h1 className="text-emerald-500 text-4xl">Hello World</h1>
   </React.StrictMode>,
 )
