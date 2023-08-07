1ae2a6335fe30285403911b90ae2149a51bcaad1
Numonu <juan.villegas.dev@gmail.com>
2023-08-07 10:20:43 -0500

    feat:create new component 'Dialog' for build custom dialogs

---
+++ src/components/molecules/Dialog.tsx
@@ -0,0 +1,23 @@
+import { ReactNode, useState } from "react";
+
+type DialogTypes = {
+	children: ReactNode;
+	offElement: ReactNode;
+	onElement?: ReactNode;
+};
+export default function Dialog({
+	children,
+	offElement,
+	onElement,
+}: DialogTypes) {
+	const [focus, setFocus] = useState(false);
+
+	const toggleFocus = () => (focus ? setFocus(false) : setFocus(true));
+
+	return (
+		<div className="relative" onClick={toggleFocus}>
+			<span className="text-3xl">{focus ? onElement : offElement}</span>
+			{focus && <div className="absolute right-0 bottom-0 translate-y-full">{children}</div>}
+		</div>
+	);
+}
