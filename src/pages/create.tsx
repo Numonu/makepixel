7770b10513128bd8bd008a3b7481c206b1763971
Numonu <juan.villegas.dev@gmail.com>
2023-08-07 20:58:32 -0500

    feat:create and test setup new page 'create'

---
+++ src/pages/create.tsx
@@ -0,0 +1,36 @@
+import Canvas from "../components/molecules/DrawCanvas";
+import Wrapper from "../components/atoms/Wrapper";
+import PaintToolBar from "../components/molecules/PaintToolBar";
+import { BsUpload } from "react-icons/bs";
+
+export default function Create() {
+	return (
+		<Wrapper>
+			<header className="text-center mt-4 mb-6">
+				<h1 className="text-black mb-2 text-xl font-medium">
+					New Pixelart
+				</h1>
+				<h2 className="text-base font-normal">
+					Create a new pixelart and contribute to Pixelcrafters
+					collection
+				</h2>
+			</header>
+			<main className="grid pb-12 gap-4 grid-cols-1 lg:grid-cols-[1fr_min-content_1fr] lg:gap-0">
+				<section className="flex justify-center order-1 lg:justify-end lg:-order-1 lg:mx-0">
+					<PaintToolBar />
+				</section>
+				<section>
+					<Canvas size={6} />
+				</section>
+				<section>
+					<button className="border-neutral-400 w-full max-w-[500px] ml-6  py-1 mx-auto flex  justify-center items-center gap-2 text-lg rounded-md border">
+						<span className="text-xl">
+							<BsUpload />
+						</span>
+						Publish
+					</button>
+				</section>
+			</main>
+		</Wrapper>
+	);
+}
