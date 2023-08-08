6654fc13f0491f2faa8c476707b26f320386db2f
Numonu <juan.villegas.dev@gmail.com>
2023-08-07 21:35:20 -0500

    update:add z index to 'GlobalHead' component

--- src/components/organisms/GlobalHead.tsx
+++ src/components/organisms/GlobalHead.tsx
@@ -7,7 +7,7 @@ import { Outlet } from "react-router-dom";
 export default function GlobalHead() {
 	return (
 		<>
-			<header className="border-neutral-100 py-2 border-b">
+			<header className="border-neutral-100 bg-white sticky top-0 py-2 border-b z-10">
 				<Wrapper className="flex gap-4 items-center md:gap-12">
 					<Brand />
 					<GallerySearch />
