50a84a91b3c2085bcb827f4848fcf2019ac74351
Numonu <juan.villegas.dev@gmail.com>
2023-08-07 20:59:14 -0500

    feat:add new page 'create' to routing in children mode

--- src/pages/routing.tsx
+++ src/pages/routing.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter , Routes , Route } from "react-router-dom";
 import GlobalHead from "../components/organisms/GlobalHead";
+import Create from "./create";
 
 export default function Routing(){
     return <BrowserRouter>
         <Routes>
             <Route path="/" element={<GlobalHead/>}>
-
+                <Route path="/create" element={<Create/>}/>
             </Route>
         </Routes>
     </BrowserRouter>
