b334ee6f5d57b888c93547d528e7cf1e34ca2a9c
Numonu <juan.villegas.dev@gmail.com>
2023-08-07 20:55:21 -0500

    feat:add outlet in 'GlobalHead' component

--- src/components/organisms/GlobalHead.tsx
+++ src/components/organisms/GlobalHead.tsx
@@ -2,15 +2,19 @@ import Brand from "../atoms/Brand";
 import GallerySearch from "../molecules/GallerySearch";
 import Wrapper from "../atoms/Wrapper";
 import NavDialog from "./NavDialog";
+import { Outlet } from "react-router-dom";
 
 export default function GlobalHead() {
 	return (
-		<header className="border-neutral-100 py-2 border-b">
-			<Wrapper className="flex gap-4 items-center md:gap-12">
-				<Brand />
-                <GallerySearch/>
-				<NavDialog/>
-			</Wrapper>
-		</header>
+		<>
+			<header className="border-neutral-100 py-2 border-b">
+				<Wrapper className="flex gap-4 items-center md:gap-12">
+					<Brand />
+					<GallerySearch />
+					<NavDialog />
+				</Wrapper>
+			</header>
+			<Outlet />
+		</>
 	);
 }
