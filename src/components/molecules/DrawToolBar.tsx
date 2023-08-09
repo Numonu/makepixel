0a106c68339e18729b1a2fee790dfdac8075ac1b
Numonu <juan.villegas.dev@gmail.com>
2023-08-09 16:18:03 -0500

    update:remove props and simplify complexity

--- src/components/molecules/DrawToolBar.tsx
+++ src/components/molecules/DrawToolBar.tsx
@@ -1,27 +1,22 @@
-import {useContext} from "react";
 import { HiOutlinePaintBrush } from "react-icons/hi2";
 import { BsEraser } from "react-icons/bs";
 import { TfiBackLeft, TfiBackRight } from "react-icons/tfi";
-import {MdInvertColors} from "react-icons/md";
+import { MdInvertColors } from "react-icons/md";
 import { MdFileDownload } from "react-icons/md";
 import ToolButton from "./ToolButton";
-import { DrawContextTypes, drawContext } from "../../global/context/drawContext";
 import { Tool } from "../../global/enums/drawEnums";
-import { requireDependencies } from "../../global/utilities/errorHandlers";
 
 export default function PaintToolBar() {
-	requireDependencies(drawContext);
-	const draw = useContext<DrawContextTypes | null>(drawContext);
 	return (
 		<aside className="flex flex-row gap-6 lg:flex-col">
 			<div className="shadow-neutral-300 p-1 rounded-md flex lg:flex-col gap-2 shadow-md">
-				<ToolButton selected={draw!.currentTool == Tool.Brush}>
+				<ToolButton toolValue={Tool.Brush}>
 					<HiOutlinePaintBrush />
 				</ToolButton>
-				<ToolButton selected={draw!.currentTool == Tool.Picker}>
-					<MdInvertColors/>
+				<ToolButton toolValue={Tool.Picker}>
+					<MdInvertColors />
 				</ToolButton>
-				<ToolButton selected={draw!.currentTool == Tool.Eraser}>
+				<ToolButton toolValue={Tool.Eraser}>
 					<BsEraser />
 				</ToolButton>
 			</div>
