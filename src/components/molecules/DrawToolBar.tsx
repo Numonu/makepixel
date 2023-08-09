a03f858caf1eefcbad8eb71ad6def67115e1a33b
Numonu <juan.villegas.dev@gmail.com>
2023-08-09 15:53:44 -0500

    update:envolve icons on new components in 'DrawToolBar'

--- src/components/molecules/DrawToolBar.tsx
+++ src/components/molecules/DrawToolBar.tsx
@@ -1,22 +1,42 @@
+import {useContext} from "react";
 import { HiOutlinePaintBrush } from "react-icons/hi2";
 import { BsEraser } from "react-icons/bs";
 import { TfiBackLeft, TfiBackRight } from "react-icons/tfi";
+import {MdInvertColors} from "react-icons/md";
 import { MdFileDownload } from "react-icons/md";
+import ToolButton from "./ToolButton";
+import { DrawContextTypes, drawContext } from "../../global/context/drawContext";
+import { Tool } from "../../global/enums/drawEnums";
+import { requireDependencies } from "../../global/utilities/errorHandlers";
 
 export default function PaintToolBar() {
+	requireDependencies(drawContext);
+	const draw = useContext<DrawContextTypes | null>(drawContext);
 	return (
 		<aside className="flex flex-row gap-6 lg:flex-col">
-			<div className="shadow-neutral-300 p-2 rounded-md flex lg:flex-col gap-4 text-3xl shadow-md">
-				<HiOutlinePaintBrush />
-				<img className="w-9" src="svg/colorpicker.svg" />
-				<BsEraser />
+			<div className="shadow-neutral-300 p-1 rounded-md flex lg:flex-col gap-2 shadow-md">
+				<ToolButton selected={draw!.currentTool == Tool.Brush}>
+					<HiOutlinePaintBrush />
+				</ToolButton>
+				<ToolButton selected={draw!.currentTool == Tool.Picker}>
+					<MdInvertColors/>
+				</ToolButton>
+				<ToolButton selected={draw!.currentTool == Tool.Eraser}>
+					<BsEraser />
+				</ToolButton>
 			</div>
-			<div className="shadow-neutral-300 p-2 rounded-md flex lg:flex-col gap-4 text-3xl shadow-md">
-				<TfiBackLeft />
-				<TfiBackRight />
+			<div className="shadow-neutral-300 p-1 rounded-md flex lg:flex-col gap-2 shadow-md">
+				<ToolButton>
+					<TfiBackLeft />
+				</ToolButton>
+				<ToolButton>
+					<TfiBackRight />
+				</ToolButton>
 			</div>
-			<div className="shadow-neutral-300 p-2 rounded-md flex lg:flex-col gap-4 text-3xl shadow-md">
-				<MdFileDownload />
+			<div className="shadow-neutral-300 p-1 rounded-md flex lg:flex-col gap-2 shadow-md">
+				<ToolButton>
+					<MdFileDownload />
+				</ToolButton>
 			</div>
 		</aside>
 	);
