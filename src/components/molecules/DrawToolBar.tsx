ad2714d6edd00ba087bd868d625b7b38e46fac09
Numonu <juan.villegas.dev@gmail.com>
2023-08-09 18:10:55 -0500

    update:update component dependencies in 'DrawToolBar' component

--- src/components/molecules/DrawToolBar.tsx
+++ src/components/molecules/DrawToolBar.tsx
@@ -3,35 +3,35 @@ import { BsEraser } from "react-icons/bs";
 import { TfiBackLeft, TfiBackRight } from "react-icons/tfi";
 import { MdInvertColors } from "react-icons/md";
 import { MdFileDownload } from "react-icons/md";
-import ToolButton from "./ToolButton";
+import ToolButton from "../atoms/ToolButton";
 import { Tool } from "../../global/enums/drawEnums";
-import ToolTip from "../ToolTip";
+import ToolTip from "../atoms/ToolTip";
 
 export default function PaintToolBar() {
 	return (
 		<aside className="flex flex-row gap-6 lg:flex-col">
 			<div className="shadow-neutral-300 p-1 rounded-md flex lg:flex-col gap-2 shadow-md">
 				<ToolTip tip="Brush" keycode="b">
-					<ToolButton toolValue={Tool.Brush}>
+					<ToolButton.Setter toolValue={Tool.Brush}>
 						<HiOutlinePaintBrush />
-					</ToolButton>
+					</ToolButton.Setter>
 				</ToolTip>
 				<ToolTip tip="Picker" keycode="P">
-					<ToolButton toolValue={Tool.Picker}>
+					<ToolButton.Setter toolValue={Tool.Picker}>
 						<MdInvertColors />
-					</ToolButton>
+					</ToolButton.Setter>
 				</ToolTip>
 				<ToolTip tip="Eraser" keycode="e">
-					<ToolButton toolValue={Tool.Eraser}>
+					<ToolButton.Setter toolValue={Tool.Eraser}>
 						<BsEraser />
-					</ToolButton>
+					</ToolButton.Setter>
 				</ToolTip>
 			</div>
 			<div className="shadow-neutral-300 p-1 rounded-md flex lg:flex-col gap-2 shadow-md">
 				<ToolTip tip="Revert" keycode="z">
-					<ToolButton>
+					<ToolButton.Setter>
 						<TfiBackLeft />
-					</ToolButton>
+					</ToolButton.Setter>
 				</ToolTip>
 
 				<ToolTip tip="Advance" keycode="x">
