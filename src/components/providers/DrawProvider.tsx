35360abe92fde4cc1c7d3a61efe32c9c87ba9e12
Numonu <juan.villegas.dev@gmail.com>
2023-08-10 11:37:10 -0500

    feat:add methods to manage snapshot and improve provider herency in 'DrawProvider'

--- src/components/providers/DrawProvider.tsx
+++ src/components/providers/DrawProvider.tsx
@@ -5,14 +5,62 @@ import { Tool } from "../../global/enums/drawEnums";
 type DrawProviderTypes = {
 	children: ReactNode;
 };
+export type SnapshotTypes = {
+	list : ImageData[],
+	current : ImageData | null,
+	listFocus : number
+}
 
 export default function DrawProvider({ children }: DrawProviderTypes) {
 	const [currentTool, setCurrentTool] = useState(Tool.Brush);
+
+	const [snapshot , setSnapshot] = useState<SnapshotTypes>({
+		list : [],
+		current : null,
+		listFocus : 0
+	});
+
+	const addSnapshot = (imageData : ImageData) => {
+		snapshot.current = imageData;
+		const newSnapshotList = [
+			...snapshot.list,
+			imageData
+		]
+		//slice and update
+		if(newSnapshotList.length > 5) newSnapshotList.shift();
+		snapshot.listFocus = newSnapshotList.length - 1;
+		setSnapshot({
+			...snapshot,
+			list : newSnapshotList
+		})
+	}
+
+	const previusSnapshot = () => {
+		setSnapshot({
+			...snapshot,
+			current : snapshot.list[snapshot.listFocus - 1]
+		})
+	}
+	const advanceSnapshot = () => {
+		setSnapshot({
+			...snapshot,
+			current : snapshot.list[snapshot.listFocus + 1]
+		})
+	}
+
 	return (
 		<drawContext.Provider
 			value={{
-				currentTool,
-				setCurrentTool,
+				tool : {
+					current : currentTool,
+					update : setCurrentTool
+				},
+				snapshot : {
+					current : snapshot.current,
+					add : addSnapshot,
+					previus : previusSnapshot,
+					advance : advanceSnapshot
+				},
 			}}
 		>
 			{children}
