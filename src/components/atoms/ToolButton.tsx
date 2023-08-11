dad35ef6b6abee8dec65fcf8a96fe3d55a9b1035
Numonu <juan.villegas.dev@gmail.com>
2023-08-11 18:10:33 -0500

    update:add click color change in 'ToolButton' component

--- src/components/atoms/ToolButton.tsx
+++ src/components/atoms/ToolButton.tsx
@@ -7,7 +7,7 @@ type ToolButtonTypes = {
 };
 export default function ToolButton({ children , callback }: ToolButtonTypes) {
 	return (
-		<button className="hover:bg-sky-200 p-2 text-2xl rounded-sm transition-colors" onClick={callback}>
+		<button className="hover:bg-sky-200 p-2 text-2xl rounded-sm transition-colors active:bg-sky-300" onClick={callback}>
 			{children}
 		</button>
 	);
