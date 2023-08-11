45bb2258b45b366fc3bde0b164bc4e26ff4d082c
Numonu <juan.villegas.dev@gmail.com>
2023-08-10 21:35:09 -0500

    feat:create all methods to manage image snapshot in 'DrawProvider' provider

--- src/components/providers/DrawProvider.tsx
+++ src/components/providers/DrawProvider.tsx
@@ -1,65 +1,72 @@
 import { ReactNode, useState } from "react";
 import { drawContext } from "../../global/context/drawContext";
 import { Tool } from "../../global/enums/drawEnums";
+import { imageDataAreSame } from "../../global/utilities/compare";
 
 type DrawProviderTypes = {
 	children: ReactNode;
 };
 export type SnapshotTypes = {
-	list : ImageData[],
-	current : ImageData | null,
-	listFocus : number
-}
+	list: ImageData[];
+	listFocus: number;
+};
 
 export default function DrawProvider({ children }: DrawProviderTypes) {
 	const [currentTool, setCurrentTool] = useState(Tool.Brush);
 
-	const [snapshot , setSnapshot] = useState<SnapshotTypes>({
-		list : [],
-		current : null,
-		listFocus : 0
+	const [snapshot, setSnapshot] = useState<SnapshotTypes>({
+		list: [],
+		listFocus: 0,
 	});
 
-	const addSnapshot = (imageData : ImageData) => {
-		snapshot.current = imageData;
-		const newSnapshotList = [
-			...snapshot.list,
-			imageData
-		]
+	const addSnapshot = (imageData: ImageData) => {
+		//does not apply if there are no changes between this one and the previous one
+		if (
+			snapshot.list.length &&
+			!imageDataAreSame(imageData, snapshot.list[snapshot.listFocus])
+		)
+			return;
+		//
+		const newSnapshotList = [...snapshot.list, imageData];
 		//slice and update
-		if(newSnapshotList.length > 5) newSnapshotList.shift();
+		if (newSnapshotList.length > 5) newSnapshotList.shift();
 		snapshot.listFocus = newSnapshotList.length - 1;
 		setSnapshot({
 			...snapshot,
-			list : newSnapshotList
-		})
-	}
+			list: newSnapshotList,
+		});
+	};
 
 	const previusSnapshot = () => {
+		const NEW_ORDER = Math.max(0, snapshot.listFocus - 1);
 		setSnapshot({
 			...snapshot,
-			current : snapshot.list[snapshot.listFocus - 1]
-		})
-	}
+			listFocus: NEW_ORDER,
+		});
+	};
 	const advanceSnapshot = () => {
+		const NEW_ORDER = Math.min(
+			snapshot.list.length - 1,
+			snapshot.listFocus + 1
+		);
 		setSnapshot({
 			...snapshot,
-			current : snapshot.list[snapshot.listFocus + 1]
-		})
-	}
+			listFocus: NEW_ORDER,
+		});
+	};
 
 	return (
 		<drawContext.Provider
 			value={{
-				tool : {
-					current : currentTool,
-					update : setCurrentTool
+				tool: {
+					current: currentTool,
+					update: setCurrentTool,
 				},
-				snapshot : {
-					current : snapshot.current,
-					add : addSnapshot,
-					previus : previusSnapshot,
-					advance : advanceSnapshot
+				snapshot: {
+					current: snapshot.list[snapshot.listFocus],
+					add: addSnapshot,
+					previus: previusSnapshot,
+					advance: advanceSnapshot,
 				},
 			}}
 		>
