8fec63a6f09737aaa76169c60622b2eb75d8b0a0
Numonu <juan.villegas.dev@gmail.com>
2023-08-12 16:35:40 -0500

    style:clean and improve the code , remove brush state dependencies

--- src/components/molecules/DrawCanvas.tsx
+++ src/components/molecules/DrawCanvas.tsx
@@ -7,7 +7,7 @@ import {
 	DrawContextTypes,
 	drawContext,
 } from "../../global/context/drawContext";
-import useCanvas, { BrushTypes, CanvasStateTypes } from "../../hooks/useCanvas";
+import useCanvas, { CanvasStateTypes } from "../../hooks/useCanvas";
 import useSnapshot from "../../hooks/useSnapshot";
 
 type DrawCanvasTypes = {
@@ -18,49 +18,50 @@ export default function DrawCanvas({ size = 8 }: DrawCanvasTypes) {
 	const canvasRef = useRef(null);
 	const draw = useContext<DrawContextTypes | null>(drawContext);
 	//
-	const [brush, setBrush] = useState<BrushTypes | null>(null);
-	const canvas: CanvasStateTypes = useCanvas(size, canvasRef, setBrush);
-	//
+	const canvas: CanvasStateTypes = useCanvas(size, canvasRef);
 	const { takeSnapshot } = useSnapshot(draw, canvas);
 	//
 	const [mouseHold, setMouseHold] = useState(false);
+	const [brush, setBrush] = useState("red");
 
 	//Execute-Action
 	const mouseAction = (x: number, y: number) => {
-		requireDependencies(canvas.element, canvas.context, brush);
-		//Pre-Config
-		const normalPos = normalizeMousePos({ x, y });
-		//Multi-Paint (for disable blurry effect)
-		repeatThis(
-			getAction({ x: normalPos.x, y: normalPos.y }, canvas.pixelSize),
-			5
-		);
+		const NORMAL = normalizeMousePos({ x, y });
+		repeatThis(getAction(NORMAL), 5);
 	};
 
 	//Normalize-Mouse-Position
 	const normalizeMousePos = (pos: Vector2) => {
-		requireDependencies(brush);
 		const CANVAS_RECT = canvas.element!.getBoundingClientRect();
+		const PIXEL_SIZE = canvas.pixelSize;
 		return {
-			x:
-				Math.floor((pos.x - CANVAS_RECT.left) / canvas.pixelSize) *
-				canvas.pixelSize,
-			y:
-				Math.floor((pos.y - CANVAS_RECT.top) / canvas.pixelSize) *
-				canvas.pixelSize,
+			x: Math.floor((pos.x - CANVAS_RECT.left) / PIXEL_SIZE) * PIXEL_SIZE,
+			y: Math.floor((pos.y - CANVAS_RECT.top) / PIXEL_SIZE) * PIXEL_SIZE,
 		};
 	};
 
 	//Return-Action-Type
-	const getAction = (pos: Vector2, size: number) => {
-		requireDependencies(draw, brush);
+	const getAction = (pos: Vector2) => {
+		const PIXEL_SIZE = canvas.pixelSize;
+
 		switch (draw!.tool.current) {
 			case Tool.Brush:
-				canvas.context!.fillStyle = brush!.color ?? "#262626";
-				return () => canvas.context!.fillRect(pos.x, pos.y, size, size);
+				canvas.context!.fillStyle = brush;
+				return () =>
+					canvas.context!.fillRect(
+						pos.x,
+						pos.y,
+						PIXEL_SIZE,
+						PIXEL_SIZE
+					);
 			case Tool.Eraser:
 				return () =>
-					canvas.context!.clearRect(pos.x, pos.y, size, size);
+					canvas.context!.clearRect(
+						pos.x,
+						pos.y,
+						PIXEL_SIZE,
+						PIXEL_SIZE
+					);
 			default:
 				return () => null;
 		}
