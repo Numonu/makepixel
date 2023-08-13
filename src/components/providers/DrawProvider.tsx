99d81f7e442df4ffe24649094ee84d146e6179cd
Numonu <juan.villegas.dev@gmail.com>
2023-08-13 18:22:27 -0500

    update:replace methods for dispatch actions in 'DrawProvider'

--- src/components/providers/DrawProvider.tsx
+++ src/components/providers/DrawProvider.tsx
@@ -1,7 +1,9 @@
-import { ReactNode, useState } from "react";
+import { ReactNode, useReducer, useState } from "react";
 import { drawContext } from "../../global/context/drawContext";
 import { Tool } from "../../global/enums/drawEnums";
-import { imageDataAreSame } from "../../global/utilities/compare";
+import {
+	snapshotReduce,
+} from "../../reducers/snapshotReduce";
 
 type DrawProviderTypes = {
 	children: ReactNode;
@@ -14,49 +16,26 @@ export type SnapshotTypes = {
 export default function DrawProvider({ children }: DrawProviderTypes) {
 	const [currentTool, setCurrentTool] = useState(Tool.Brush);
 
-	const [snapshot, setSnapshot] = useState<SnapshotTypes>({
+	const [snapshot, dispatch] = useReducer(snapshotReduce, {
 		list: [],
 		listFocus: 0,
 	});
 
 	const addSnapshot = (imageData: ImageData) => {
-		if(snapshot.list.length == 0){
-			//UPDATE
-			setSnapshot({
-				...snapshot,
-				list: [imageData],
-			});
-			return;
-		}
-		//WE CHECK THAT THEY ARE DIFFERENT
-		const IMG_ARE_SAME = imageDataAreSame(imageData, snapshot.list[snapshot.listFocus]);
-		if(IMG_ARE_SAME) return;
-		//SLICE
-		const newSnapshotList = [...snapshot.list, imageData];
-		if (newSnapshotList.length > 10) newSnapshotList.shift();
-		//UPDATE
-		snapshot.listFocus = newSnapshotList.length - 1;
-		setSnapshot({
-			...snapshot,
-			list: newSnapshotList,
+		dispatch({
+			type: "add",
+			value: imageData,
 		});
 	};
 
 	const previusSnapshot = () => {
-		const NEW_ORDER = Math.max(0, snapshot.listFocus - 1);
-		setSnapshot({
-			...snapshot,
-			listFocus: NEW_ORDER,
+		dispatch({
+			type: "previus",
 		});
 	};
 	const advanceSnapshot = () => {
-		const NEW_ORDER = Math.min(
-			snapshot.list.length - 1,
-			snapshot.listFocus + 1
-		);
-		setSnapshot({
-			...snapshot,
-			listFocus: NEW_ORDER,
+		dispatch({
+			type: "advance",
 		});
 	};
 
