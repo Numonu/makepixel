d83d344697e1b0060fb0591a710cfc77dcd7e5c9
Numonu <juan.villegas.dev@gmail.com>
2023-08-14 17:27:00 -0500

    feat:create component for manage tool options display 'ToolOptions'

---
+++ src/components/ToolOptions.tsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { drawContext } from "../global/context/drawContext";
+import { Tool } from "../global/enums/drawEnums";
+import BrushOptions from "./BrushOptions";
+
+export default function ToolOptions() {
+	const draw = useContext(drawContext);
+	if (!draw) return null;
+	switch (draw.tool.current) {
+		case Tool.Brush:
+			return <BrushOptions/>;
+        case Tool.Eraser:
+            return <h1>Wait...</h1>
+		default:
+            return <h1>Wait...</h1>
+	}
+}
