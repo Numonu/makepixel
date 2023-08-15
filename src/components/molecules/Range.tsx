bdb044a0d982dbf7487387ff02ebcf9b5a8bff00
Numonu <juan.villegas.dev@gmail.com>
2023-08-15 11:37:35 -0500

    feat:the 'Range' component now use state from provider

--- src/components/molecules/Range.tsx
+++ src/components/molecules/Range.tsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
-
 type RangeTypes = {
+	current: number;
 	min?: number;
 	max?: number;
 	onChange: (value: number) => void;
 };
-export default function Range({ min = 1, max = 10, onChange }: RangeTypes) {
-	const [value, setValue] = useState(0);
-
+export default function Range({
+	min = 1,
+	max = 10,
+	current,
+	onChange,
+}: RangeTypes) {
 	const updateValue = (newValue: string) => {
 		const VALUE = Number(newValue);
-		//
 		onChange(VALUE);
-		setValue(VALUE);
 	};
 
 	return (
@@ -20,12 +20,12 @@ export default function Range({ min = 1, max = 10, onChange }: RangeTypes) {
 			<input
 				className="accent-sky-300"
 				type="range"
-				value={value}
+				value={current}
 				min={min}
 				max={max}
 				onChange={(e) => updateValue(e.target.value)}
 			/>
-			<span className="text-neutral-700">{value}</span>
+			<span className="text-neutral-700">{current}</span>
 		</div>
 	);
 }
