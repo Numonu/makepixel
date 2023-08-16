28a0d7c1c1f0e4a3991f967e0f72f2ba1338ab1d
Numonu <juan.villegas.dev@gmail.com>
2023-08-16 17:32:48 -0500

    feat:the colors history now updates current color after click in 'RecentColors' component

--- src/components/molecules/RecentColors.tsx
+++ src/components/molecules/RecentColors.tsx
@@ -3,17 +3,23 @@ import { drawContext } from "../../global/context/drawContext";
 
 export default function RecentColors() {
 	const draw = useContext(drawContext);
+
+	const pickThisColor = (newColor:string) => {
+		draw!.color.update(newColor);
+	}
+
 	return (
 		<div className="flex flex-wrap gap-2">
 			{draw!.color.history.map((e,i) => {
 				return (
-					<div
+					<button
                         key={i}
-						className="w-5 aspect-square rounded-full"
+						className="w-6 aspect-square rounded-full hover:scale-110 cursor-pointer transition-transform duration-75"
+						onClick={() => pickThisColor(e)}
 						style={{
 							backgroundColor: e,
 						}}
-					></div>
+					></button>
 				);
 			})}
 		</div>
