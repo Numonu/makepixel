d13dd6fb3460fc58731a97037a862b2ec2154e22
Numonu <juan.villegas.dev@gmail.com>
2023-08-15 21:29:00 -0500

    feat:add feature for update color from input in 'BrushOptions' component

--- src/components/organisms/BrushOptions.tsx
+++ src/components/organisms/BrushOptions.tsx
@@ -7,6 +7,7 @@ export default function BrushOptions() {
 	const draw = useContext(drawContext);
 
 	const updateColor = (newColor: string) => {
+		console.log(newColor);
 		draw!.color.update(newColor);
 	};
 
@@ -17,9 +18,10 @@ export default function BrushOptions() {
 				<div className="bg-neutral-100 border-neutral-100 w-[200px] mb-2 p-1 pl-2 border rounded-md flex items-center gap-1">
 					<span className="text-neutral-500 text-xl">#</span>
 					<input
-						value={draw!.color.current.replace("#", "")}
-						className="bg-transparent min-w-0 outline-none tracking-widest"
 						type="text"
+						className="bg-transparent min-w-0 outline-none tracking-widest"
+						value={draw!.color.current.replace("#", "")}
+						onChange={(e) => updateColor("#" + e.target.value)}
 					/>
 					<CopyButton target={draw!.color.current} />
 				</div>
