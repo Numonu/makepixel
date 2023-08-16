20b3092a24f13c94cd80197723e53f0a01876a41
Numonu <juan.villegas.dev@gmail.com>
2023-08-16 18:36:11 -0500

    update:change name of component 'ToolButton'

--- src/components/atoms/ToolCall.tsx
+++ src/components/atoms/ToolCall.tsx
@@ -1,10 +1,10 @@
 import { ReactNode } from "react";
 
-type ToolButtonTypes = {
+type ToolCallTypes = {
 	children: ReactNode;
 	callback? : () => void;
 };
-export default function ToolButton({ children , callback }: ToolButtonTypes) {
+export default function ToolCall({ children , callback }: ToolCallTypes) {
 	return (
 		<button className="hover:bg-sky-200 p-2 text-2xl rounded-sm transition-colors active:bg-sky-300" onClick={callback}>
 			{children}
