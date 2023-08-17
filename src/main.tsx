2b83bab9b972872af0abcc61c7216cb0fad267e7
Numonu <juan.villegas.dev@gmail.com>
2023-08-17 09:34:26 -0500

    feat:render in the 'main' app the 'Toaster' component

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./global/styles/defaults.css";
 import "./global/styles/animations.css";
 import Routing from "./pages/routing";
+import { Toaster } from "sonner";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<div className="bg-white text-neutral-900">
+			<Toaster/>
 			<Routing />
 		</div>
 	</React.StrictMode>
