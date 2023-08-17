835c8e70a3df4fdc0c9126863443f83069490c05
Numonu <juan.villegas.dev@gmail.com>
2023-08-17 09:23:21 -0500

    feat:the global search input is visible only in the main page 'GallerySearch'

--- src/components/molecules/GallerySearch.tsx
+++ src/components/molecules/GallerySearch.tsx
@@ -1,8 +1,14 @@
 import { BsSearch } from "react-icons/bs";
+import { useLocation } from "react-router-dom";
 
 export default function GallerySearch() {
+	const location = useLocation();
+	const INVISIBLE = location.pathname !== "/";
 	return (
-		<div className="border-neutral-500 grow py-1 px-2 border flex gap-2 items-center rounded-full">
+		<div
+			className={`border-neutral-500 grow py-1 px-2 border flex gap-2 items-center rounded-full 
+			${INVISIBLE && "opacity-0"}`}
+		>
 			<span className="text-neutral-500">
 				<BsSearch />
 			</span>
