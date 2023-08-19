e492ff17cda58fb58b2600fd65e2226d17d31e8b
Numonu <juan.villegas.dev@gmail.com>
2023-08-19 16:55:58 -0500

    update:add gap to 'GridConfig' component

--- src/components/GridConfig.tsx
+++ src/components/GridConfig.tsx
@@ -4,7 +4,7 @@ export default function GridConfig() {
 	return (
 		<section>
 			<h3 className="font-medium mb-2">Canvas Size</h3>
-			<div className="w-1/2 grid grid-cols-4">
+			<div className="w-1/2 grid grid-cols-4 gap-1">
 				<GridSetter value={8}/>
 				<GridSetter value={16}/>
 				<GridSetter value={32}/>
