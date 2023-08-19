b0a6e8457f455f7d1262051e22e6f8b4403f6360
Numonu <juan.villegas.dev@gmail.com>
2023-08-18 21:14:20 -0500

    feat:create new component 'DownloadTool' for download canvas image

---
+++ src/components/molecules/DownloadTool.tsx
@@ -0,0 +1,29 @@
+import ToolTip from "../atoms/ToolTip";
+import {BsDownload} from "react-icons/bs";
+import ToolCall from "../atoms/ToolCall";
+import RemoteAction from "../atoms/RemoteAction";
+import { CANVAS_ID } from "../../global/constant/DrawConstant";
+
+export default function DownloadTool() {
+    const downloadCanvasImage = () => {
+        const canvas = document.getElementById(CANVAS_ID) as HTMLCanvasElement;
+        if(canvas){
+            const imageUrl = canvas.toDataURL();
+            const tempAnchor = document.createElement("a");
+            tempAnchor.href = imageUrl;
+            tempAnchor.download = "created_on_pixelcrafters.png";
+            tempAnchor.click();
+        }
+        else{
+            throw new Error(`[!]No canvas element found with id ${CANVAS_ID}`);
+        }
+    }   
+	return (
+		<ToolTip tip="Download" keycode="d">
+			<ToolCall callback={downloadCanvasImage}>
+				<BsDownload />
+				<RemoteAction keycode="d" />
+			</ToolCall>
+		</ToolTip>
+	);
+}
