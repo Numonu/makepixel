d7fed49dfa6d661fad6832c08d8dc9903dee475c
Numonu <juan.villegas.dev@gmail.com>
2023-08-20 18:18:07 -0500

    add constats values for draw tags in 'DrawConstant'

--- src/global/constant/DrawConstant.tsx
+++ src/global/constant/DrawConstant.tsx
@@ -1,5 +1,16 @@
 const CANVAS_ID = "drawCanvas";
 
+const DRAW_TAGS = [
+    "character",
+    "landscape",
+    "weapon",
+    "abstract",
+    "creature",
+    "lettering",
+    "props",
+    "other"
+]
 export {
-    CANVAS_ID
+    CANVAS_ID,
+    DRAW_TAGS
 }
