83ab1e459a6b6f48f9d9e6dd56b6433bb8c443bd
Numonu <juan.villegas.dev@gmail.com>
2023-08-20 17:05:29 -0500

    update:update component dependencies in 'FunctionalModal'

--- src/components/molecules/FunctionalModal.tsx
+++ src/components/molecules/FunctionalModal.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import { MdOutlineCancel } from "react-icons/md";
-import Modal from "../Modal";
+import Modal from "./Modal";
 
 type ModalTypes = {
 	children: ReactNode;
