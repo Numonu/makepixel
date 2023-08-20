976e6394d549a98c37fd636afed1d5621a91f0fe
Numonu <juan.villegas.dev@gmail.com>
2023-08-20 17:00:05 -0500

    feat:create new component based on modal 'FunctionModal'

---
+++ src/components/molecules/FunctionalModal.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from "react";
+import { MdOutlineCancel } from "react-icons/md";
+import Modal from "../Modal";
+
+type ModalTypes = {
+	children: ReactNode;
+	title: string;
+	subtitle: string;
+	onClose: () => void;
+};
+export default function FunctionalModal({
+	children,
+	title,
+	subtitle,
+	onClose,
+}: ModalTypes) {
+	return (
+		<Modal onClose={onClose}>
+			<header className="flex justify-between">
+				<div>
+					<h2 className="text-neutral-900 font-medium text-xl mb-2">
+						{title}
+					</h2>
+					<p className="text-neutral-700">{subtitle}</p>
+				</div>
+				<span
+					className="text-neutral-500 text-3xl cursor-pointer"
+					onClick={onClose}
+				>
+					<MdOutlineCancel />
+				</span>
+			</header>
+			<hr className="my-3" />
+			<section>{children}</section>
+		</Modal>
+	);
+}
