3ea5ca92cb45664081a2fa32e9b35aca95382521
Numonu <juan.villegas.dev@gmail.com>
2023-08-21 16:47:11 -0500

    feat:create new component for art posts 'GridGallery'

---
+++ src/components/organisms/GridGallery.tsx
@@ -0,0 +1,19 @@
+import ArtCard from "./ArtCard";
+
+export default function GridGallery(){
+    return <main className="grid grid-cols-4 gap-6">
+        <ArtCard/>
+        <ArtCard/>
+        <ArtCard/>
+        <ArtCard/>
+        <ArtCard/>
+        <ArtCard/>
+        <ArtCard/>
+        <ArtCard/>
+        <ArtCard/>
+        <ArtCard/>
+        <ArtCard/>
+        <ArtCard/>
+        <ArtCard/>
+    </main>
+}
