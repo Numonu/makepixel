9c9bd75bbc74dd4eb87174a403f57f7d95e4e390
Numonu <juan.villegas.dev@gmail.com>
2023-08-21 11:38:47 -0500

    feat:add new page gallery to routing

--- src/pages/routing.tsx
+++ src/pages/routing.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter , Routes , Route } from "react-router-dom";
 import GlobalHead from "../components/organisms/GlobalHead";
 import Create from "./create";
+import Gallery from "./gallery";
 
 export default function Routing(){
     return <BrowserRouter>
         <Routes>
             <Route path="/" element={<GlobalHead/>}>
+                <Route index element={<Gallery/>}/>
                 <Route path="/create" element={<Create/>}/>
             </Route>
         </Routes>
