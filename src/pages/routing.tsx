9e826856fd8a941f2d50a3f82d63b184d9d34eb7
Numonu <juan.villegas.dev@gmail.com>
2023-08-21 16:46:31 -0500

    feat:add new routes and nestings in 'routing'

--- src/pages/routing.tsx
+++ src/pages/routing.tsx
@@ -2,12 +2,16 @@ import { BrowserRouter , Routes , Route } from "react-router-dom";
 import GlobalHead from "../components/organisms/GlobalHead";
 import Create from "./create";
 import Gallery from "./gallery";
+import Post from "./post";
 
 export default function Routing(){
     return <BrowserRouter>
         <Routes>
             <Route path="/" element={<GlobalHead/>}>
                 <Route index element={<Gallery/>}/>
+                <Route path="/gallery" element={<Gallery/>}>
+                    <Route path="/gallery/:post" element={<Post/>}/>
+                </Route>
                 <Route path="/create" element={<Create/>}/>
             </Route>
         </Routes>
