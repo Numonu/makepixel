090d5c08ffa0e635b5c1512ff366e0250ea60156
Numonu <juan.villegas.dev@gmail.com>
2023-08-22 12:14:06 -0500

    feat:add focus effects and labels to 'Input' component from auth

--- src/pages/auth/components/atoms/Input.tsx
+++ src/pages/auth/components/atoms/Input.tsx
@@ -1,19 +1,37 @@
-import {useState} from "react";
+import { useState } from "react";
 
 type InputTypes = {
-    placeholder:string
-    type?:string
-}
+	placeholder: string;
+	type?: string;
+};
 
-export default function Input({type = "text" , placeholder}:InputTypes){
+export default function Input({ type = "text", placeholder }: InputTypes) {
+	const [focus, setFocus] = useState(false);
 
-    const [focus , setFocus] = useState(false);
+	const on = () => setFocus(true);
+	const off = () => setFocus(false);
 
-    const on = () => setFocus(true);
-    const off = () => setFocus(false);
-
-    return <label htmlFor={placeholder + "_"} className={`pb-1 px-2 flex gap-2 border-b transition-colors ${focus && "border-primary"}`} onFocus={on} onBlur={off}>
-        <span className={`capitalize transition-colors ${focus && "text-primary"}`}>{placeholder}</span>
-        <input id={placeholder + "_"} className="grow outline-none" type={type} />
-    </label>
+	return (
+		<label
+			className={`text-neutral-700 pb-1 px-2 flex gap-4 border-b transition-colors ${
+				focus && "border-primary"
+			}`}
+			htmlFor={placeholder + "_"}
+			onFocus={on}
+			onBlur={off}
+		>
+			<span
+				className={`capitalize transition-colors ${
+					focus && "text-primary"
+				}`}
+			>
+				{placeholder}
+			</span>
+			<input
+				className="text-title grow outline-none text-base"
+				id={placeholder + "_"}
+				type={type}
+			/>
+		</label>
+	);
 }
