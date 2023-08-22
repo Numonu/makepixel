c25d7958f31e1fa685a1dcf5efff010651eda167
Numonu <juan.villegas.dev@gmail.com>
2023-08-22 17:32:59 -0500

    update:wrap all pages to user provider in 'routing'

--- src/pages/routing.tsx
+++ src/pages/routing.tsx
@@ -1,21 +1,26 @@
-import { BrowserRouter , Routes , Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Create from "./create/create";
 import Gallery from "./gallery/gallery";
 import Post from "./post/post";
 import Auth from "./auth/auth";
 import GlobalHead from "../global/components/organisms/GlobalHead";
+import UserProvider from "../global/provider/UserProvider";
 
-export default function Routing(){
-    return <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<GlobalHead/>}>
-                <Route index element={<Gallery/>}/>
-                <Route path="/gallery" element={<Gallery/>}>
-                    <Route path="/gallery/:post" element={<Post/>}/>
-                </Route>
-                <Route path="/create" element={<Create/>}/>
-            </Route>
-            <Route path="/auth" element={<Auth/>}/>
-        </Routes>
-    </BrowserRouter>
+export default function Routing() {
+	return (
+		<UserProvider>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<GlobalHead />}>
+						<Route index element={<Gallery />} />
+						<Route path="/gallery" element={<Gallery />}>
+							<Route path="/gallery/:post" element={<Post />} />
+						</Route>
+						<Route path="/create" element={<Create />} />
+					</Route>
+					<Route path="/auth" element={<Auth />} />
+				</Routes>
+			</BrowserRouter>
+		</UserProvider>
+	);
 }
