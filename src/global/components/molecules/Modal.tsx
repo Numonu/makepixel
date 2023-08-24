b90d9de595e61155a722858ec4568bde8fed8a88
Numonu <juan.villegas.dev@gmail.com>
2023-08-24 13:09:59 -0500

    feat:add className prop to 'Modal' component

--- src/global/components/molecules/Modal.tsx
+++ src/global/components/molecules/Modal.tsx
@@ -2,17 +2,18 @@ import { ReactNode } from "react";
 import { createPortal } from "react-dom";
 
 type ModalTypes = {
+	className?: string;
 	children: ReactNode;
 	onClose: () => void;
 };
-export default function Modal({ children, onClose }: ModalTypes) {
+export default function Modal({ children, className, onClose }: ModalTypes) {
 	return createPortal(
 		<div
 			className="bg-black bg-opacity-70 animate-visible fixed left-0 top-0 w-screen h-screen py-12 flex justify-center backdrop-blur-sm z-30 overflow-y-scroll"
 			onClick={onClose}
 		>
 			<div
-				className="bg-neutral-50 w-[90%] h-max max-w-[600px] p-4 rounded-md"
+				className={`bg-white w-[90%] h-max max-w-[600px] p-4 rounded-md ${className}`}
 				onClick={(e) => e.stopPropagation()}
 			>
 				{children}
