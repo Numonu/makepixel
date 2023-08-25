163c15501867aea785f3dc39210e3814b1504f59
Numonu <juan.villegas.dev@gmail.com>
2023-08-25 18:35:00 -0500

    delete:delete unused component 'EffectSize'

--- src/pages/create/components/molecules/EffectSize.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { drawContext } from "../../global/context/drawContext";
-import Range from "./Range";
-
-export default function EffectSize() {
-	const draw = useContext(drawContext);
-	return <Range current={draw!.effectSize.current} onChange={(e) => draw!.effectSize.update(e)} />;
-}
