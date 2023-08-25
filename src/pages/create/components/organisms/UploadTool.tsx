314a4bfa4f1a61c79e8d96af475f9c8bbfa5c36e
Numonu <juan.villegas.dev@gmail.com>
2023-08-24 22:01:26 -0500

    feat:remove all logic from 'UploadTool'

--- src/pages/create/components/organisms/UploadTool.tsx
+++ src/pages/create/components/organisms/UploadTool.tsx
@@ -1,70 +1,21 @@
-import { useState } from "react";
 import { BsCloudUpload } from "react-icons/bs";
-import ToolCall from "../atoms/ToolCall";
 import ToolTip from "../../../../global/components/atoms/ToolTip";
+import ToolCall from "../atoms/ToolCall";
+import UploadModal from "./UploadModal";
 import RemoteAction from "../../../../global/components/atoms/RemoteAction";
-import FunctionalModal from "../../../../global/components/molecules/FunctionalModal";
-import Tag from "../atoms/Tag";
-import { PiWarningOctagonLight } from "react-icons/pi";
-import { DRAW_TAGS } from "../../../../global/constants/DrawConstant";
-import To from "../../../../global/components/atoms/To";
+import useModal from "../../../../global/hooks/useModal";
 
 export default function UploadTool() {
-	const [showModal, setShowModal] = useState(false);
-	const [selectTag, setSelectTag] = useState("other");
-
-	const on = () => setShowModal(true);
-	const off = () => setShowModal(false);
-
-	const updateTag = (newTag: string) => setSelectTag(newTag);
-
+	const { modal, openModal, closeModal } = useModal();
 	return (
 		<>
 			<ToolTip tip="Upload" keycode="u">
-				<ToolCall callback={on}>
+				<ToolCall callback={openModal}>
 					<BsCloudUpload />
 					<RemoteAction keycode="u" />
 				</ToolCall>
 			</ToolTip>
-			{showModal && (
-				<FunctionalModal
-					title="Upload"
-					subtitle="Share your art with thousands of other users and pixelart lovers."
-					onClose={off}
-				>
-					<form>
-						<input
-							required
-							type="text"
-							placeholder="Name your masterpiece"
-							className="bg-neutral-200 w-1/2 p-2 mb-4 rounded-md "
-						/>
-						<div className="mb-12">
-							<h3 className="mb-4">Select a tag</h3>
-							<div className="flex flex-wrap gap-4">
-								{DRAW_TAGS.map((e) => (
-									<Tag
-										color="gray"
-										value={e}
-										onClick={updateTag}
-										selected={selectTag == e}
-									/>
-								))}
-							</div>
-						</div>
-						<button className="bg-sky-400 text-white py-2 px-6 mb-2 rounded-md font-medium hover:bg-sky-500">
-							Publish
-						</button>
-						<span className="flex items-center gap-1 text-xs">
-							<span className="inline-block text-base">
-								<PiWarningOctagonLight />
-							</span>
-							By posting on pixelcrafters I am agreeing to the{" "}
-							<To to="/">Terms and Conditions</To>
-						</span>
-					</form>
-				</FunctionalModal>
-			)}
+			{modal && <UploadModal onClose={closeModal} />}
 		</>
 	);
 }
