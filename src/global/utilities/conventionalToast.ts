4c341e202671173edbc070878ed92ca21d62c264
Numonu <juan.villegas.dev@gmail.com>
2023-08-25 17:40:47 -0500

    update:add new toast messages in 'conventionalToast'

--- src/global/utilities/conventionalToast.ts
+++ src/global/utilities/conventionalToast.ts
@@ -1,9 +1,15 @@
 import { toast } from "sonner";
 
+//constantes
 const FAIL_MESSAGE = "something went wrong";
+const SUCCES_MESSAGE = "success";
+const LOAD_MESSAGE = "loading...";
+//metodos
 const errorToast = () => toast.error(FAIL_MESSAGE);
 
 export {
     FAIL_MESSAGE,
+    SUCCES_MESSAGE,
+    LOAD_MESSAGE,
     errorToast
 }
