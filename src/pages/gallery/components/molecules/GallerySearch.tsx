67497584753ce8e4345e7cc6873227f95f4ddcf5
Numonu <juan.villegas.dev@gmail.com>
2023-08-26 13:06:12 -0500

    feat:the component 'GallerySearch' is rendered only in gallery page

--- src/pages/gallery/components/molecules/GallerySearch.tsx
+++ src/pages/gallery/components/molecules/GallerySearch.tsx
@@ -4,16 +4,15 @@ import { useLocation } from "react-router-dom";
 export default function GallerySearch() {
 	const location = useLocation();
 	const INVISIBLE = location.pathname !== "/";
+	if(INVISIBLE) return null;
 	return (
 		<div
-			className={`border-neutral-500 grow py-1 px-2 border flex gap-2 items-center rounded-full 
-			${INVISIBLE && "opacity-0"}`}
-		>
+			className="border-neutral-500 grow py-1 px-2 border flex gap-2 items-center rounded-full">
 			<span className="text-neutral-500">
 				<BsSearch />
 			</span>
 			<input
-				className="grow bg-transparent outline-none"
+				className="w-full grow bg-transparent outline-none"
 				type="text"
 				placeholder="Search Pixelarts"
 			/>
