d150110591e9c69836ff0fda91d3524df43da465
Numonu <juan.villegas.dev@gmail.com>
2023-08-27 11:22:18 -0500

    feat:create new custom hook to load user bio data from storage 'useBio'

---
+++ src/pages/profile/hooks/useBio.tsx
@@ -0,0 +1,46 @@
+import { doc, getDoc } from "firebase/firestore";
+import { loadStorage, saveStorage } from "../utilities/storage";
+import { useState, useEffect } from "react";
+import { db } from "../../../lib/firebase.config";
+
+export type DataTypes = {
+	username: string;
+	description: string;
+	link: {
+		instagram: string;
+		youtube: string;
+	};
+};
+export default function useBio(uid: string) {
+	const [bioData, setBioData] = useState(loadStorage(uid));
+
+	//En caso de no tener datos locales lo cargaremos de firestore
+	useEffect(() => {
+		if (!bioData) {
+			getDoc(doc(db, "users", uid)).then((result) => {
+				if (result.exists()) {
+					const DATA = result.data() as DataTypes;
+					saveStorage(uid!, DATA);
+					updateBio(DATA);
+				} else throw new Error("[!]User not found");
+			});
+		}
+	}, [bioData, uid]);
+
+	//Actualza el estado con los datos de la biografia del usuario
+	const updateBio = (newData: DataTypes) => {
+		setBioData({
+			username: newData.username,
+			description: newData.description,
+			link: {
+				instagram: newData.link.instagram,
+				youtube: newData.link.youtube,
+			},
+		});
+	};
+
+	return {
+		bioData,
+		setBioData,
+	};
+}
