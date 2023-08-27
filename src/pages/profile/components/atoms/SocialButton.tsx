a62092f8165f8ac09759a8d18d883ed406eebd48
Numonu <juan.villegas.dev@gmail.com>
2023-08-27 12:46:38 -0500

    feat:add new props to setup 'SocialButton'

--- src/pages/profile/components/atoms/SocialButton.tsx
+++ src/pages/profile/components/atoms/SocialButton.tsx
@@ -3,17 +3,19 @@ import { ReactNode } from "react";
 type SocialButtonTypes = {
 	icon: ReactNode;
 	children: ReactNode;
-	href?: string;
+	user: string;
+	link: string;
 };
 
 export default function SocialButton({
 	children,
 	icon,
-	href,
+	user,
+	link,
 }: SocialButtonTypes) {
 	return (
 		<a
-			href={href ?? "#"}
+			href={link + user ?? "#"}
 			className="border-title text-title w-max p-2 flex items-center gap-2 border rounded-lg hover:border-primary hover:text-primary transition-colors"
 		>
 			<span className="text-2xl">{icon}</span>
