489c58cc9f934cc0ee833112da1ca4822fc92cc1
Numonu <juan.villegas.dev@gmail.com>
2023-08-27 20:55:52 -0500

    feat:add new methods to session storage in 'storage'

--- src/pages/profile/utilities/storage.ts
+++ src/pages/profile/utilities/storage.ts
@@ -6,5 +6,13 @@ const loadStorage = (key: string) => {
 	if (storage) return JSON.parse(storage);
 	return null;
 };
+const saveSession = (key: string, value: object) => {
+	sessionStorage[key] = JSON.stringify(value);
+};
+const loadSession = (key: string) => {
+	const storage = sessionStorage[key];
+	if (storage) return JSON.parse(storage);
+	return null;
+};
 
-export { saveStorage, loadStorage };
+export { saveStorage, loadStorage, saveSession, loadSession };
