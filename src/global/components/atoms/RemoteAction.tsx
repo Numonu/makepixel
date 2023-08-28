8af0baf51c42704d03d9f58dce850e163cbd7002
Numonu <juan.villegas.dev@gmail.com>
2023-08-27 21:50:45 -0500

    feat:component 'RemoteAction' is only enabled if context allows

--- src/global/components/atoms/RemoteAction.tsx
+++ src/global/components/atoms/RemoteAction.tsx
@@ -1,18 +1,20 @@
-import { useEffect, useRef } from "react";
+import { useContext, useEffect, useRef } from "react";
+import { remoteContext } from "../../provider/context/remoteContext";
 
 type RemoteActionTypes = {
 	keycode: string;
 };
 export default function RemoteAction({ keycode }: RemoteActionTypes) {
+	const { remotes } = useContext(remoteContext);
 	const remote = useRef(null);
 
 	useEffect(() => {
 		const CURRENT = remote.current! as HTMLSpanElement;
 		const HANDLE_KEY = (e: KeyboardEvent) => {
-			if (e.key == keycode) CURRENT.click();
+			if (remotes && e.key == keycode) CURRENT.click();
 		};
 		addEventListener("keydown", HANDLE_KEY);
 		return () => removeEventListener("keydown", HANDLE_KEY);
-	}, [keycode]);
+	}, [keycode, remotes]);
 	return <span ref={remote}></span>;
 }
