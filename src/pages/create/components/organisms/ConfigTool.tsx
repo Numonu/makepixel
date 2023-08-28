c58a1a1174138f71bfe3b18a4206ea21865a9024
Numonu <juan.villegas.dev@gmail.com>
2023-08-27 21:49:38 -0500

    feat:some tool modal components modify remote actions state

--- src/pages/create/components/organisms/ConfigTool.tsx
+++ src/pages/create/components/organisms/ConfigTool.tsx
@@ -1,30 +1,38 @@
-import { useState } from "react";
+import { useContext } from "react";
 import { BsNut } from "react-icons/bs";
 import ToolCall from "../atoms/ToolCall";
 import ToolTip from "../../../../global/components/atoms/ToolTip";
 import RemoteAction from "../../../../global/components/atoms/RemoteAction";
 import FunctionalModal from "../../../../global/components/molecules/FunctionalModal";
 import GridSetter from "../molecules/GridSetter";
+import { remoteContext } from "../../../../global/provider/context/remoteContext";
+import useModal from "../../../../global/hooks/useModal";
 
 export default function ConfigTool() {
-	const [showModal, setShowModal] = useState(false);
-
-	const on = () => setShowModal(true);
-	const off = () => setShowModal(false);
+	const { enableRemotes, disableRemotes } = useContext(remoteContext);
+	const { modal, closeModal, openModal } = useModal();
 
 	return (
 		<>
 			<ToolTip tip="Config" keycode="c">
-				<ToolCall callback={on}>
+				<ToolCall
+					callback={() => {
+						openModal();
+						disableRemotes();
+					}}
+				>
 					<BsNut />
 					<RemoteAction keycode="c" />
 				</ToolCall>
 			</ToolTip>
-			{showModal && (
+			{modal && (
 				<FunctionalModal
 					title="Settings"
 					subtitle="Configure your workspace"
-					onClose={off}
+					onClose={() => {
+						closeModal();
+						enableRemotes();
+					}}
 				>
 					<h3 className="font-medium mb-2">Canvas Size</h3>
 					<div className="w-1/2 grid grid-cols-4 gap-1">
--- src/pages/create/components/organisms/UploadTool.tsx
+++ src/pages/create/components/organisms/UploadTool.tsx
@@ -4,18 +4,33 @@ import ToolCall from "../atoms/ToolCall";
 import UploadModal from "./UploadModal";
 import RemoteAction from "../../../../global/components/atoms/RemoteAction";
 import useModal from "../../../../global/hooks/useModal";
+import { useContext } from "react";
+import { remoteContext } from "../../../../global/provider/context/remoteContext";
 
 export default function UploadTool() {
+	const { disableRemotes, enableRemotes } = useContext(remoteContext);
 	const { modal, openModal, closeModal } = useModal();
 	return (
 		<>
 			<ToolTip tip="Upload" keycode="u">
-				<ToolCall callback={openModal}>
+				<ToolCall
+					callback={() => {
+						openModal();
+						disableRemotes();
+					}}
+				>
 					<BsCloudUpload />
 					<RemoteAction keycode="u" />
 				</ToolCall>
 			</ToolTip>
-			{modal && <UploadModal onClose={closeModal} />}
+			{modal && (
+				<UploadModal
+					onClose={() => {
+						closeModal();
+						enableRemotes();
+					}}
+				/>
+			)}
 		</>
 	);
 }
