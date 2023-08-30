c7f97bcbfa7277841773e4c78f04e893bd411d0c
Numonu <juan.villegas.dev@gmail.com>
2023-08-30 11:26:30 -0500

    update:remove outlet from 'gallery' page

--- src/pages/gallery/gallery.tsx
+++ src/pages/gallery/gallery.tsx
@@ -1,13 +1,12 @@
-
-import { Outlet } from "react-router-dom";
 import Wrapper from "../../global/components/atoms/Wrapper";
 import GalleryAside from "./components/organisms/GalleryAside";
 import GridGallery from "./components/organisms/GridGallery";
 
-export default function Gallery(){
-    return <Wrapper className="pt-4 grid grid-cols-[max-content_1fr] gap-12">
-        <GalleryAside/>
-        <GridGallery/>
-        <Outlet/>
-    </Wrapper>
+export default function Gallery() {
+	return (
+		<Wrapper className="pt-4 grid grid-cols-[max-content_1fr] gap-12">
+			<GalleryAside/>
+			<GridGallery/>
+		</Wrapper>
+	);
 }
