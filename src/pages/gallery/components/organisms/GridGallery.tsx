df81bf6b969b822dc30c775e2545df63e4757f6a
Numonu <juan.villegas.dev@gmail.com>
2023-08-30 16:09:45 -0500

    feat:add tag and timeline filter to 'GridGallery' component

--- src/pages/gallery/components/organisms/GridGallery.tsx
+++ src/pages/gallery/components/organisms/GridGallery.tsx
@@ -1,19 +1,57 @@
+import { useState, useEffect } from "react";
 import ArtCard from "./ArtCard";
+import { collection, getDocs, query, orderBy } from "firebase/firestore";
+import { db } from "../../../../config/firebase.config";
+import { ArtDataTypes } from "../../../../global/constants/types";
+import ArtCardSoul from "../molecules/ArtCardSoul";
+import Repeat from "../../../../global/components/atoms/Repeat";
+import { useParams } from "react-router-dom";
 
-export default function GridGallery(){
-    return <main className="grid grid-cols-4 gap-6">
-        <ArtCard/>
-        <ArtCard/>
-        <ArtCard/>
-        <ArtCard/>
-        <ArtCard/>
-        <ArtCard/>
-        <ArtCard/>
-        <ArtCard/>
-        <ArtCard/>
-        <ArtCard/>
-        <ArtCard/>
-        <ArtCard/>
-        <ArtCard/>
-    </main>
+export default function GridGallery() {
+	const { filter, tag } = useParams();
+	const [arts, setArts] = useState<ArtDataTypes[] | null>(null);
+
+	const getSort = () => {
+		switch (filter) {
+			case "new":
+				return orderBy("timestamp", "desc");
+			case "top":
+				return orderBy("likes", "desc");
+			default:
+				return orderBy("timestamp", "desc");
+		}
+	};
+
+	useEffect(() => {
+		const q = query(collection(db, "gallery"), getSort());
+		getDocs(q).then((queryResult) => {
+			const result: ArtDataTypes[] = [];
+			queryResult.forEach((e) => result.push(e.data() as ArtDataTypes));
+			setArts(result);
+			console.log(result);
+		});
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [filter]);
+
+	if (!arts)
+		return (
+			<main className="grid grid-cols-4 gap-6">
+				<Repeat repeat={16}>
+					<ArtCardSoul />
+				</Repeat>
+			</main>
+		);
+
+	return (
+		<main className="grid grid-cols-4 gap-6">
+			{arts
+				.filter((e) => {
+					if (!tag) return true;
+					else return e.tag == tag;
+				})
+				.map((e) => (
+					<ArtCard data={e} />
+				))}
+		</main>
+	);
 }
