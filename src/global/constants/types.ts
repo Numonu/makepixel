3a00b1e1b6af15b2ea413dabb46cd81d5c99b446
Numonu <juan.villegas.dev@gmail.com>
2023-08-30 20:41:24 -0500

    feat:add new fields to art data types in 'types'

--- src/global/constants/types.ts
+++ src/global/constants/types.ts
@@ -2,9 +2,10 @@ import { FieldValue } from "firebase/firestore";
 
 type ArtDataTypes = {
 	title: string;
-	likes: number;
+	likes: string[];
 	tag: string;
 	uid: string;
+	id: string;
 	timestamp: FieldValue | string;
 	name: string;
 	url: string;
