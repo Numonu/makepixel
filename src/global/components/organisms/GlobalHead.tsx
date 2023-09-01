b1b39ac779e45201e13a59d0c9620a16cf5a7df4
Numonu <juan.villegas.dev@gmail.com>
2023-09-01 15:10:15 -0500

    delete:delete component 'GallerySearch'

--- src/global/components/organisms/GlobalHead.tsx
+++ src/global/components/organisms/GlobalHead.tsx
@@ -1,5 +1,4 @@
 import Brand from "../atoms/Brand";
-import GallerySearch from "../../../pages/gallery/components/molecules/GallerySearch";
 import Wrapper from "../atoms/Wrapper";
 import NavDialog from "./NavDialog";
 import { Outlet } from "react-router-dom";
@@ -10,7 +9,6 @@ export default function GlobalHead() {
 			<header className="border-neutral-100 bg-white sticky top-0 py-2 border-b z-10">
 				<Wrapper className="flex gap-4 justify-between items-center md:gap-12">
 					<Brand />
-					<GallerySearch />
 					<NavDialog />
 				</Wrapper>
 			</header>
