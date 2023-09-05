7e9c5aadb9127abdd36cf02f1dfa06c0cab77c9c
Numonu <juan.villegas.dev@gmail.com>
2023-09-05 12:15:56 -0500

    update:prevent translate in 'SocialButton' component

--- src/pages/profile/components/atoms/SocialButton.tsx
+++ src/pages/profile/components/atoms/SocialButton.tsx
@@ -19,7 +19,9 @@ export default function SocialButton({
 			className="border-title text-title w-max p-2 flex items-center gap-2 border rounded-lg hover:border-primary hover:text-primary transition-colors"
 		>
 			<span className="text-2xl">{icon}</span>
-			{children}
+			<span translate="no">
+				{children}
+			</span>
 		</a>
 	);
 }
