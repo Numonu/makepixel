0defd14a67edeab405e621fc14b9e8f9e4e0f5bf
Numonu <juan.villegas.dev@gmail.com>
2023-09-06 10:44:49 -0500

    feat:add dynamic filters and better settings for configuration in 'usePagination'

--- src/pages/gallery/hooks/usePagination.tsx
+++ src/pages/gallery/hooks/usePagination.tsx
@@ -1,6 +1,7 @@
 import {
-    DocumentData,
+	DocumentData,
 	QueryDocumentSnapshot,
+	QueryFieldFilterConstraint,
 	QueryOrderByConstraint,
 	collection,
 	getDocs,
@@ -17,17 +18,27 @@ type UsePaginationTypes = (
 	setter: React.Dispatch<React.SetStateAction<ArtDataTypes[] | null>>,
 	configuration: {
 		queryLenght: number;
-		sortMode: () => QueryOrderByConstraint;
-		onPaginate? : (data:ArtDataTypes[]) => void;
+		sortMode?: QueryOrderByConstraint;
+		whereMode?: QueryFieldFilterConstraint;
+		onPaginate?: (data: ArtDataTypes[]) => void;
 	}
 ) => {
-    fullArts : boolean,
-    paginationSoul : boolean,
-    setLastDocument : React.Dispatch<React.SetStateAction<QueryDocumentSnapshot<DocumentData, DocumentData> | null>>,
-    setFullArts :  React.Dispatch<React.SetStateAction<boolean>>,
+	fullArts: boolean;
+	paginationSoul: boolean;
+	setLastDocument: React.Dispatch<
+		React.SetStateAction<QueryDocumentSnapshot<
+			DocumentData,
+			DocumentData
+		> | null>
+	>;
+	setFullArts: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-export const usePagination: UsePaginationTypes = (state, setter, configuration) => {
+export const usePagination: UsePaginationTypes = (
+	state,
+	setter,
+	configuration
+) => {
 	//Estado para guardar el ultimo documento obtenido
 	const [lastDocument, setLastDocument] =
 		useState<null | QueryDocumentSnapshot>(null);
@@ -36,6 +47,34 @@ export const usePagination: UsePaginationTypes = (state, setter, configuration) 
 	//Estado para controlar los esqueletos de carga
 	const [paginationSoul, setPaginationSoul] = useState(false);
 
+	//Metodo para construir nuestra query
+	const getQuery = (
+		sortMode?: QueryOrderByConstraint,
+		whereMode?: QueryFieldFilterConstraint
+	) => {
+		const getTemplate = (inyect: QueryOrderByConstraint | QueryFieldFilterConstraint) =>
+			query(collection(db, "gallery"),inyect,startAfter(lastDocument),limit(configuration.queryLenght));
+
+		if (sortMode && whereMode) {
+			return query(
+				collection(db, "gallery"),
+				whereMode,
+				sortMode,
+				startAfter(lastDocument),
+				limit(configuration.queryLenght)
+			);
+		} else if (sortMode) {
+			return getTemplate(sortMode);
+		} else if (whereMode) {
+			return getTemplate(whereMode);
+		}
+		return query(
+			collection(db, "gallery"),
+			startAfter(lastDocument),
+			limit(configuration.queryLenght)
+		);
+	};
+
 	useEffect(() => {
 		//Este metodo se llama al llegar al final de la pagina
 		const handleScroll = () => {
@@ -43,22 +82,15 @@ export const usePagination: UsePaginationTypes = (state, setter, configuration) 
 				window.innerHeight + window.scrollY >=
 				document.body.offsetHeight;
 
-			//Si no hemos llegado al final 
-            //o no tenemos un ultimo documento nos retiramos
+			//Si no hemos llegado al final
+			//o no tenemos un ultimo documento nos retiramos
 			if (!isScrolledToBottom || !lastDocument) return;
-
 
 			//Activamos el esqueleto y preparamos la query para la peticion
 			setPaginationSoul(true);
-			const q = query(
-				collection(db, "gallery"),
-				configuration.sortMode(),
-				startAfter(lastDocument),
-				limit(configuration.queryLenght)
-			);
+			const q = getQuery(configuration.sortMode , configuration.whereMode);
 
-
-            //Exito en el pedido , ahora convertimos el documento en un objeto
+			//Exito en el pedido , ahora convertimos el documento en un objeto
 			getDocs(q).then((queryResult) => {
 				const result: ArtDataTypes[] = [];
 				queryResult.forEach((e) =>
@@ -71,7 +103,8 @@ export const usePagination: UsePaginationTypes = (state, setter, configuration) 
 					setter(fusion);
 
 					//Si tenemos un callback lo llamamos
-					if(configuration.onPaginate) configuration.onPaginate(fusion);
+					if (configuration.onPaginate)
+						configuration.onPaginate(fusion);
 
 					//Designamos el ultimo obtenido para usarlo de punto de inicio en la siguiente paginacion
 					const last = queryResult.docs[queryResult.docs.length - 1];
@@ -81,9 +114,9 @@ export const usePagination: UsePaginationTypes = (state, setter, configuration) 
 					setPaginationSoul(false);
 
 					//No hay mas datos que pedir , cancelamos futuros esqueletos
-					if (result.length < configuration.queryLenght){
-                        setFullArts(true);
-                    }
+					if (result.length < configuration.queryLenght) {
+						setFullArts(true);
+					}
 				} else {
 					//No hay mas datos que pedir, quitamos el esqueleto actual y cancelamos los siguientes
 					setPaginationSoul(false);
@@ -99,11 +132,10 @@ export const usePagination: UsePaginationTypes = (state, setter, configuration) 
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [lastDocument]);
 
-    return {
-        fullArts,
-        setFullArts,
-        setLastDocument,
-        paginationSoul,
-    }
+	return {
+		fullArts,
+		setFullArts,
+		setLastDocument,
+		paginationSoul,
+	};
 };
-
