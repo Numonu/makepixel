3f6b7846e0ebe3c995002d220bf148a0abc9317e
Numonu <juan.villegas.dev@gmail.com>
2023-09-10 12:50:23 -0500

    feat:add conditional link to create page in 'GlobalHead' component

--- src/global/components/organisms/GlobalHead.tsx
+++ src/global/components/organisms/GlobalHead.tsx
@@ -1,7 +1,9 @@
 import Brand from "../atoms/Brand";
 import Wrapper from "../atoms/Wrapper";
 import NavDialog from "./NavDialog";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import { BsPlusCircleFill } from "react-icons/bs";
+import AttachToRoute from "../atoms/AttachToRoute";
 
 export default function GlobalHead() {
 	return (
@@ -9,7 +11,20 @@ export default function GlobalHead() {
 			<header className="border-neutral-100 bg-white sticky top-0 py-2 border-b z-10">
 				<Wrapper className="flex gap-4 justify-between items-center md:gap-12">
 					<Brand />
-					<NavDialog />
+					<div className="flex gap-6 items-center">
+						<AttachToRoute atach="/">
+							<Link
+								className="bg-primary text-white border-secondary py-1 px-2 flex gap-2 items-center rounded-md border-b-2 hover:border-0"
+								to="/create"
+							>
+								<span className="text-xl">
+									<BsPlusCircleFill />
+								</span>
+								Create Pixel Art
+							</Link>
+						</AttachToRoute>
+						<NavDialog />
+					</div>
 				</Wrapper>
 			</header>
 			<Outlet />
