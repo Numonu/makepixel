9126a622fc0ed687900b68b1dca261db65536a7f
Numonu <juan.villegas.dev@gmail.com>
2023-09-14 22:17:45 -0500

    update:add slot to put temp components for events and more

--- src/pages/gallery/gallery.tsx
+++ src/pages/gallery/gallery.tsx
@@ -1,12 +1,18 @@
 import Wrapper from "../../global/components/atoms/Wrapper";
+import Aseprite from "../../temp/Aseprite";
 import GalleryAside from "./components/organisms/GalleryAside";
 import GridGallery from "./components/organisms/GridGallery";
 
 export default function Gallery() {
 	return (
-		<Wrapper className="pt-4 grid grid-cols-1 gap-12 md:px-4 lg:grid-cols-[min-content_1fr]">
-			<GalleryAside/>
-			<GridGallery/>
-		</Wrapper>
+		<>
+			{/* // Espacio para anuncios propios */}
+			<Aseprite />
+			{/* // */}
+			<Wrapper className="pt-4 grid grid-cols-1 gap-12 md:px-4 lg:grid-cols-[min-content_1fr]">
+				<GalleryAside />
+				<GridGallery />
+			</Wrapper>
+		</>
 	);
 }
